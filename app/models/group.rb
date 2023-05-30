9df94676b8bb91ef869e3d22fad65e3aca1046b8
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-30 17:36:20 -0400

    Add method to get sim of group

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,8 @@ class Group < ApplicationRecord
   has_and_belongs_to_many :entities
 
   validates :name, :icon, presence: true
+
+  def sum_group
+    entities.map(&:amount).reduce(:+)&.round(2) || 0
+  end
 end
