56edc0a49ac2939c622384393fe44631149d9cd4
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-30 16:56:24 -0400

    Add user_id key in group migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_191037) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_205055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,11 +23,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_191037) do
     t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
+  create_table "entities_groups", id: false, force: :cascade do |t|
+    t.bigint "entity_id", null: false
+    t.bigint "group_id", null: false
+  end
+
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
