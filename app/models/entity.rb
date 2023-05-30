fb41f116522111ea3a38365cba346ef13a235b2b
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-30 14:46:42 -0400

    Add many to many association for entities and groups

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,6 +1,6 @@
 class Entity < ApplicationRecord
   belongs_to :author, foreign_key: :user_id, class_name: 'User'
-  has_many :groups
+  has_and_belongs_to_many :groups
 
   validates :name, :amount, presence: true
 end
