afc8ece4d055c94797ba059e9c3c8b2321a7752d
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-05-31 17:29:23 -0400

    Add test for entity model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'John Doe', email: 'john@example.com', password: 'password') }
+  let(:entity1) { Entity.create(name: 'Entity 1', amount: 10) }
+  let(:entity2) { Entity.create(name: 'Entity 2', amount: 20) }
+  let(:group) { Group.create(name: 'Test Group', icon: 'test-icon', user_id: user.id) }
+
 end
